897a558f4e447929f3d0a10bd65bfc0cd23a4105
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2025-11-05 16:18:22 -0300

    landing-page-final

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.menu-toggle')
+
+const nav = document.querySelector('.nav-links')
+
+menu.addEventListener('click', () =>{
+
+    nav.classList.toggle('active')
+})
