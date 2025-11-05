8af2b23582997d554cf8cdba33be93a955671fde
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2025-11-05 16:33:54 -0300

    correção de bug

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const menu = document.querySelector('.menu-toggle')
 
 const nav = document.querySelector('.nav-links')
 
+const links = document.querySelectorAll('.nav-links a')
+
 menu.addEventListener('click', () =>{
 
     nav.classList.toggle('active')
+})
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('active')
+    })
 })
